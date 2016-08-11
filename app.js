d813cdddf737f7725aff509d67a7d398c7f88cdb
Raymond Do <cheapazn@hotmail.com>
2016-08-10 21:01:35 -0700

    Fixed JS crappy math by adding rounding and parsing to float.

--- app.js
+++ app.js
@@ -22,37 +22,40 @@ var pikePlaceMarket = {
   },
   cupHr: function() {
     for (var i = 0; i < hours.length; i++) {
-      this.numCupHr[i] = this.numCustHr[i] * this.avgCup;
+      this.numCupHr[i] = parseFloat((this.numCustHr[i] * this.avgCup).toFixed(2));
     }
   },
   totalCupCalc: function() {
     for (var i = 0; i < hours.length; i++) {
       this.totalCup += this.numCupHr[i];
+      this.totalCup = parseFloat(this.totalCup.toFixed(2));
     }
   },
   pndHr: function() {
     for (var i = 0; i < hours.length; i++) {
-      this.numPndHr[i] = this.numCustHr[i] * this.avgPnd;
+      this.numPndHr[i] = parseFloat((this.numCustHr[i] * this.avgPnd).toFixed(2));
     }
   },
   totalPndCalc: function() {
     for (var i = 0; i < hours.length; i++) {
       this.totalPnd += this.numPndHr[i];
+      this.totalPnd = parseFloat(this.totalPnd.toFixed(2));
     }
   },
   PndPerCup: function() {
     for (var i = 0; i < hours.length; i++) {
-      this.numPndPerCup[i] = this.numCupHr[i] / 16;
+      this.numPndPerCup[i] = parseFloat((this.numCupHr[i] / 16).toFixed(2));
     }
   },
   netPndCalc: function() {
     for (var i = 0; i < hours.length; i++) {
-      this.netPnd[i] = this.numPndHr[i] + this.numPndPerCup[i];
+      this.netPnd[i] = parseFloat((this.numPndHr[i] + this.numPndPerCup[i]).toFixed(2));
     }
   },
   totalNetPndCalc: function() {
     for (var i = 0; i < hours.length; i++) {
       this.totalNetPnd += this.netPnd[i];
+      this.totalNetPnd = parseFloat(this.totalNetPnd.toFixed(2));
     }
   },
   totalCustCalc: function() {
@@ -75,7 +78,8 @@ pikePlaceMarket.totalCustCalc();
 
 // Adding the Pike Place information to data.html
 var sectionEl = document.getElementsByTagName('section');
-console.log(sectionEl);
 var ulEl = document.createElement('ul');
 ulEl.textContent = pikePlaceMarket.name;
 sectionEl[0].appendChild(ulEl);
+var ilEl = document.createElement('li');
+ilEl.textContent = '6:00am: 86.4 lbs [23 customers, 27.6 cups (1.4 lbs), 85 lbs to-go]';
